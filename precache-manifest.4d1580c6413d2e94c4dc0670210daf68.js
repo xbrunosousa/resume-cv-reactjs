0ae8a79d28a5d75a967597eda89189b871020eba
Bruno Sousa <xbrunosousa@icloud.com>
2020-02-10 13:10:55 -0300

    Updates

---
+++ precache-manifest.4d1580c6413d2e94c4dc0670210daf68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ff11ff8ec10a10179760be75f85ef8",
+    "url": "/resume-cv-reactjs/index.html"
+  },
+  {
+    "revision": "7100c9e560d1fae0ffbb",
+    "url": "/resume-cv-reactjs/static/css/main.d7d529db.chunk.css"
+  },
+  {
+    "revision": "80b7efa0c8bb7d88bab3",
+    "url": "/resume-cv-reactjs/static/js/2.2d930d35.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/resume-cv-reactjs/static/js/2.2d930d35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7100c9e560d1fae0ffbb",
+    "url": "/resume-cv-reactjs/static/js/main.045ab6c6.chunk.js"
+  },
+  {
+    "revision": "5c0ecd8ea46db3af3494",
+    "url": "/resume-cv-reactjs/static/js/runtime-main.aab6b396.js"
+  }
+]);
